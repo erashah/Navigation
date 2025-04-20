9679bc5fb0035224bd2ca327c50f7d1ec5852977
Era Shah <erashah001@gmail.com>
2025-04-20 19:20:03 -0400

    Adding swiftUi pages for the second and third view

---
+++ Navigation/secondPage.swift
@@ -0,0 +1,18 @@
+//
+//  secondPage.swift
+//  Navigation
+//
+//  Created by Era Shah on 4/20/25.
+//
+
+import SwiftUI
+
+struct secondPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    secondPage()
+}
---
+++ Navigation/thirdPage.swift
@@ -0,0 +1,18 @@
+//
+//  thirdPage.swift
+//  Navigation
+//
+//  Created by Era Shah on 4/20/25.
+//
+
+import SwiftUI
+
+struct thirdPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    thirdPage()
+}
